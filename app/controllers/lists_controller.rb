0e8499d27701f051b89cf1c2be0196293d1cb935
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-10-18 12:52:51 +0000

    [Add]sample_appテスト投稿

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,20 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
 
-  def index
+  def create
+
+    list = List.new(list_params)
+
+    list.save
+
+    redirect_to '/top'
+
   end
 
-  def show
+  private
+    def list_params
+      params.require(:list).permit(:title, :body)
+    end
   end
-
-  def edit
-  end
-end
