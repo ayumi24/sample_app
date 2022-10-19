2176ede1a08ff81346083aee7cfd26ff0208b74f
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-10-19 11:27:47 +0000

    [Finish]編集機能を作ろう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,6 +26,12 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
   private
     def list_params
       params.require(:list).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
   get  'lists' => 'lists#index'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit',as: 'edit_list'
   get '/top' => 'homes#top'
   get 'lists/:id' => 'lists#show',as: 'list'
+  patch 'lists/:id' => 'lists#update',as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
