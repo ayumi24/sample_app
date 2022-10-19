8277fb8b065c7974cc934414dd854d1c2ae237fd
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-10-19 10:45:37 +0000

    [Finish]詳細画面を作ろう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,7 +9,7 @@ class ListsController < ApplicationController
 
     list.save
 
-    redirect_to '/top'
+    redirect_to list_path(list.id)
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get  'lists' => 'lists#index'
   get 'lists/edit'
   get '/top' => 'homes#top'
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show',as: 'list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
