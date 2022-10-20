cf2fa298bbe58cc9a20afb2816c8711b9536c806
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-10-20 11:05:59 +0000

    [Finish]バリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get  'lists' => 'lists#index'
-  get 'lists/:id/edit' => 'lists#edit',as: 'edit_list'
-  get '/top' => 'homes#top'
-  get 'lists/:id' => 'lists#show',as: 'list'
-  patch 'lists/:id' => 'lists#update',as: 'update_list'
+  get 'top' => 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
