6f5505268054421ba73c5d17aa69a516d48dec03
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-10-23 01:26:53 +0000

    [Add]フラッシュメッセージを実装してみよう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,7 @@ class ListsController < ApplicationController
 
     @list = List.new(list_params)
     if @list.save
+      flash[:notice]="投稿が成功しました"
       redirect_to list_path(@list.id)
     else
       render :new
